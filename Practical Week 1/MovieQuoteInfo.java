a098b50760efa0930d8299d6cb7183956b65454b
rico97 <andrico.cahyadi@my.jcu.edu.au>
2016-11-15 14:29:43 +0800

    Practical Week 1 Added

---
+++ Practical Week 1/MovieQuoteInfo.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Rico on 11/15/2016.
+ */
+public class MovieQuoteInfo
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Said by most actor");
+    }
+}
