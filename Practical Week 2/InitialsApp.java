52164d7e6674623da5e3b9c7cb62e6a244c1fb62
rico97 <andrico.cahyadi@my.jcu.edu.au>
2016-11-22 23:02:37 +0800

    Practical Week 2 Added

---
+++ Practical Week 2/InitialsApp.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Rico on 11/22/2016.
+ */
+public class InitialsApp {
+    public static void main (String[] args)
+    {
+        Initials in = new Initials();
+        in.displayInitials("Andrico Cahyadi GAGAGGA");
+        in.displayInitials("Donald Trump BEBEBEB");
+    }
+}
